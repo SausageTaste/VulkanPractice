bd9f9ccbcfbf475a66e24525ea23f330a3a0365f
Sungmin Woo <woos8899@gmail.com>
2020-03-03 16:22:11 +0900

    Window creation by class.

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,3 +1,5 @@
+#include <exception>
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -8,32 +10,53 @@
 
 #include <iostream>
 
-int main() {
-    glfwInit();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+class Window {
 
-    {
-        uint32_t extensionCount = 0;
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+private:
+    const char* const WINDOW_TITLE = "Vulkan Practice";
 
-        std::cout << extensionCount << " extensions supported" << std::endl;
+    GLFWwindow* m_window = nullptr;
+
+public:
+    Window(const int width, const int height) {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        this->m_window = glfwCreateWindow(width, height, this->WINDOW_TITLE, nullptr, nullptr);
+        if (nullptr == this->m_window) {
+            throw std::runtime_error{ "Failed to create window." };
+        }
+
+        {
+            uint32_t extensionCount = 0;
+            vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+            std::cout << extensionCount << " extensions supported" << std::endl;
+        }
     }
 
-    {
-        glm::mat4 matrix;
-        glm::vec4 vec;
-        auto test = matrix * vec;
+    ~Window(void) {
+        glfwDestroyWindow(this->m_window);
+        this->m_window = nullptr;
+        glfwTerminate();
     }
 
-    while (!glfwWindowShouldClose(window)) {
+    void update(void) {
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
+    bool isOughtToClose(void) {
+        return glfwWindowShouldClose(this->m_window);
+    }
 
-    glfwTerminate();
+};
+
+
+int main() {
+    Window window{ 800, 450 };
+
+    while ( !window.isOughtToClose() ) {
+        window.update();
+    }
 
     return 0;
 }
