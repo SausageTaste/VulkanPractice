dfa4537fdcd9007cd3df10e541e1f52825043943
Sungmin Woo <woos8899@gmail.com>
2020-03-08 04:00:51 +0900

    Commented out glm inclusion codes.

--- apps/vkwindow.cpp
+++ apps/vkwindow.cpp
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <exception>
 
+/*
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
+*/
 
 
 namespace {
