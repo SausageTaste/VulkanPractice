f371ee97d9c4258dfeed80385ba417a4a871e214
Sungmin Woo <woos8899@gmail.com>
2020-03-08 02:45:47 +0900

    Vulkan instance creatiing implemented.

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,4 +1,5 @@
 #include <exception>
+#include <vector>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -11,32 +12,41 @@
 #include <iostream>
 
 
-class Window {
+class VulkanWindowGLFW {
 
 private:
     const char* const WINDOW_TITLE = "Vulkan Practice";
 
     GLFWwindow* m_window = nullptr;
+    VkInstance m_instance = nullptr;
 
 public:
-    Window(const int width, const int height) {
-        glfwInit();
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-        this->m_window = glfwCreateWindow(width, height, this->WINDOW_TITLE, nullptr, nullptr);
-        if (nullptr == this->m_window) {
-            throw std::runtime_error{ "Failed to create window." };
-        }
-
+    VulkanWindowGLFW(const int width, const int height) {
         {
-            uint32_t extensionCount = 0;
-            vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-            std::cout << extensionCount << " extensions supported" << std::endl;
+            glfwInit();
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            this->m_window = glfwCreateWindow(width, height, this->WINDOW_TITLE, nullptr, nullptr);
+            if ( nullptr == this->m_window ) {
+                throw std::runtime_error{ "Failed to create window." };
+            }
+        }
+       
+        {
+            const auto appInfo = this->makeAppInfo();
+            const auto createInfo = this->makeInstCreateInfo(appInfo);
+
+            if ( VK_SUCCESS != vkCreateInstance(&createInfo, nullptr, &this->m_instance) ) {
+                throw std::runtime_error{ "Failed to create vulkan instance. " };
+            }
         }
     }
 
-    ~Window(void) {
+    ~VulkanWindowGLFW(void) {
+        vkDestroyInstance(this->m_instance, nullptr);
+        this->m_instance = nullptr;
+
         glfwDestroyWindow(this->m_window);
         this->m_window = nullptr;
         glfwTerminate();
@@ -50,52 +60,64 @@ public:
         return glfwWindowShouldClose(this->m_window);
     }
 
-};
-
-
-class VulkanInstance {
-
 private:
-    VkInstance m_instance;
+    static VkApplicationInfo makeAppInfo(void) {
+        VkApplicationInfo appInfo = {};
 
-public:
-    VulkanInstance(void) {
-        VkApplicationInfo appInfo;
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vulkan Practice";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        return appInfo;
+    }
+
+    static VkInstanceCreateInfo makeInstCreateInfo(const VkApplicationInfo& appInfo) {
+        VkInstanceCreateInfo createInfo = {};
+
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
         {
-            appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-            appInfo.pApplicationName = "Vulkan Practice";
-            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-            appInfo.pEngineName = "No Engine";
-            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-            appInfo.apiVersion = VK_API_VERSION_1_0;
-        }
-
-        VkInstanceCreateInfo createInfo;
-        {
-            createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-            createInfo.pApplicationInfo = &appInfo;
-
             uint32_t glfwExtensionCount = 0;
             const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
             createInfo.enabledExtensionCount = glfwExtensionCount;
             createInfo.ppEnabledExtensionNames = glfwExtensions;
-
-            createInfo.enabledLayerCount = 0;
         }
 
-        if ( VK_SUCCESS != vkCreateInstance(&createInfo, nullptr, &this->m_instance) ) {
-            throw std::runtime_error{ "Failed to create vulkan instance. "};
+        createInfo.enabledLayerCount = 0;
+
+        return createInfo;
+    }
+
+    static void printVulkanExtensions(void) {
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions are following\n";
+        for ( const auto& ext : extensions ) {
+            std::cout << "    " << ext.extensionName << '\n';
         }
     }
 
-}
+};
 
 
-int main() {
-    Window window{ 800, 450 };
+int main(int argc, char* argv) {
+    try {
+        VulkanWindowGLFW window{ 800, 450 };
 
-    while ( !window.isOughtToClose() ) {
-        window.update();
+        while ( !window.isOughtToClose() ) {
+            window.update();
+        }
+    }
+    catch ( const std::exception & e ) {
+        std::cout << "Fatal Exception: " << e.what() << std::endl;
     }
 
     return 0;
