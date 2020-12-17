f775f74d72c4994f51f9752286f8229a4175d130
Sungmin Woo <woos8899@gmail.com>
2020-12-17 22:47:49 +0900

    Seperate function `createBuffer`

--- apps/vert_data.cpp
+++ apps/vert_data.cpp
@@ -18,6 +18,36 @@ namespace {
         throw std::runtime_error("failed to find suitable memory type!");
     }
 
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties,
+        VkBuffer& buffer, VkDeviceMemory& bufferMemory, VkDevice logiDevice, VkPhysicalDevice physDevice)
+    {
+        VkBufferCreateInfo bufferInfo{};
+        bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        bufferInfo.size = size;
+        bufferInfo.usage = usage;
+        bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+        if (vkCreateBuffer(logiDevice, &bufferInfo, nullptr, &buffer) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create buffer!");
+        }
+
+        VkMemoryRequirements memRequirements;
+        vkGetBufferMemoryRequirements(logiDevice, buffer, &memRequirements);
+
+        VkMemoryAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        allocInfo.allocationSize = memRequirements.size;
+        allocInfo.memoryTypeIndex = ::findMemType(
+            memRequirements.memoryTypeBits, properties, physDevice
+        );
+
+        if (vkAllocateMemory(logiDevice, &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate buffer memory!");
+        }
+
+        vkBindBufferMemory(logiDevice, buffer, bufferMemory, 0);
+    }
+
 }
 
 
@@ -66,40 +96,20 @@ namespace dal {
 namespace dal {
 
     void VertexBuffer::init(const std::vector<Vertex>& vertices, const VkDevice logiDevice, const VkPhysicalDevice physDevice) {
-        VkBufferCreateInfo bufInfo{};
-        bufInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-        bufInfo.size = sizeof(vertices[0]) * vertices.size();
-        bufInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-        bufInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-
-        if (VK_SUCCESS != vkCreateBuffer(logiDevice, &bufInfo, nullptr, &buffer)) {
-            throw std::runtime_error("failed to create vertex buffer!");
-        }
-        if (VK_NULL_HANDLE == this->buffer) {
-            throw std::runtime_error("created vertex buffer but it is still NULL!");
-        }
-
-        VkMemoryRequirements memRequirements{};
-        vkGetBufferMemoryRequirements(logiDevice, this->buffer, &memRequirements);
-
-        VkMemoryAllocateInfo allocInfo{};
-        allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-        allocInfo.allocationSize = memRequirements.size;
-        allocInfo.memoryTypeIndex = ::findMemType(
-            memRequirements.memoryTypeBits,
+        VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
+        ::createBuffer(
+            bufferSize,
+            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            this->buffer,
+            this->buffer_mem,
+            logiDevice,
             physDevice
         );
 
-        if (VK_SUCCESS != vkAllocateMemory(logiDevice, &allocInfo, nullptr, &this->buffer_mem)) {
-            throw std::runtime_error("failed to allocate vertex buffer memory!");
-        }
-
-        vkBindBufferMemory(logiDevice, this->buffer, this->buffer_mem, 0);
-
         void* data;
-        vkMapMemory(logiDevice, this->buffer_mem, 0, bufInfo.size, 0, &data);
-        memcpy(data, vertices.data(), static_cast<size_t>(bufInfo.size));
+        vkMapMemory(logiDevice, this->buffer_mem, 0, bufferSize, 0, &data);
+        memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
         vkUnmapMemory(logiDevice, this->buffer_mem);
 
         this->vertSize = static_cast<uint32_t>(vertices.size());
