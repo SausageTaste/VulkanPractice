5723d7397686ad1165656ad0795f08370d298671
Sungmin Woo <woos8899@gmail.com>
2021-01-23 23:13:08 +0900

    Init native activity

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,27 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_sausagetaste_vulkanpractice_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#include "android_native_app_glue.h"
+
+
+extern "C" {
+
+void handle_cmd(android_app *pApp, int32_t cmd) {
+
+}
+
+void android_main(struct android_app *pApp) {
+    pApp->onAppCmd = handle_cmd;
+
+    int events;
+    android_poll_source *pSource;
+    do {
+        if (ALooper_pollAll(0, nullptr, &events, (void **) &pSource) >= 0) {
+            if (pSource) {
+                pSource->process(pApp, pSource);
+            }
+        }
+    } while (!pApp->destroyRequested);
+}
+
 }
