f1f65d96b33d9fb685abae9ba1d6f3b6278f9fc4
Sungmin Woo <woos8899@gmail.com>
2021-01-23 23:25:56 +0900

    Print some cmd

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,13 +1,55 @@
 #include <jni.h>
 #include <string>
 
-#include "android_native_app_glue.h"
+#include <android/log.h>
+#include <android_native_app_glue.h>
 
 
 extern "C" {
 
-void handle_cmd(android_app *pApp, int32_t cmd) {
+void handle_cmd(android_app* const state, const int32_t cmd) {
+    switch (cmd) {
+        case APP_CMD_INIT_WINDOW:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: init window");
+            break;
+        case APP_CMD_WINDOW_RESIZED:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: init window");
+            break;
+        case APP_CMD_DESTROY:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: destroy");
+            break;
+        case APP_CMD_PAUSE:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: pause");
+            break;
+        case APP_CMD_RESUME:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: resume");
+            break;
+        case APP_CMD_START:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: start");
+            break;
+        case APP_CMD_STOP:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: stop");
+            break;
+        case APP_CMD_GAINED_FOCUS:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: focus gained");
+            break;
+        case APP_CMD_LOST_FOCUS:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: focus lost");
+            break;
+        case APP_CMD_INPUT_CHANGED:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: input changed");
+            break;
+        case APP_CMD_TERM_WINDOW:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: window terminated");
+            break;
+        case APP_CMD_SAVE_STATE:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd: save state");
+            break;
+        default:
+            __android_log_print(ANDROID_LOG_VERBOSE, "vulkan_practice", "handle cmd; unknown (%d)", cmd);
+            break;
 
+    }
 }
 
 void android_main(struct android_app *pApp) {
